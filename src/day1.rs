c73139595b09dc9c46ba231fe3cc59e46de699c2
Christian Duvholt <christian@duvholt.net>
2025-12-01 06:36:54 +0100

    Quick cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -1,24 +1,14 @@
 pub fn part1(contents: String) -> String {
     let mut dial = 50;
 
-    dbg!(&contents);
-
     let mut zero = 0;
 
     for line in contents.lines() {
-        let left = line.starts_with('L');
-        let count: i64 = line
-            .trim_start_matches("L")
-            .trim_start_matches("R")
-            .parse()
-            .unwrap();
+        let (left, clicks) = parse(line);
+        let summable_click = if left { -clicks } else { clicks };
 
-        let val = if left { -count } else { count };
+        dial = (dial + summable_click).rem_euclid(100);
 
-        dbg!(dial);
-        dial = (dial + val).rem_euclid(100);
-
-        println!("Left {}, count {}", left, count);
         if dial == 0 {
             zero += 1;
         }
@@ -27,29 +17,33 @@ pub fn part1(contents: String) -> String {
     zero.to_string()
 }
 
+fn parse(line: &str) -> (bool, i64) {
+    let left = line.starts_with('L');
+    let count: i64 = line
+        .trim_start_matches("L")
+        .trim_start_matches("R")
+        .parse()
+        .unwrap();
+    (left, count)
+}
+
 pub fn part2(contents: String) -> String {
-    let mut dial = 50;
+    let mut dial_position = 50;
 
     let mut zero = 0;
 
     for line in contents.lines() {
-        let left = line.starts_with('L');
-        let count: i64 = line
-            .trim_start_matches("L")
-            .trim_start_matches("R")
-            .parse()
-            .unwrap();
+        let (left, clicks) = parse(line);
+        let summable_click = if left { -clicks } else { clicks };
 
-        let val = if left { -count } else { count };
-
-        let full_rounds = count / 100;
-        let new_dial = dial + (val % 100);
-        if (new_dial <= 0 && dial > 0) || new_dial >= 100 {
+        let full_rounds = clicks / 100;
+        let new_dial = dial_position + (summable_click % 100);
+        if (new_dial <= 0 && dial_position > 0) || new_dial >= 100 {
             zero += 1;
         }
         zero += full_rounds;
 
-        dial = (dial + val).rem_euclid(100);
+        dial_position = (dial_position + summable_click).rem_euclid(100);
     }
 
     zero.to_string()
@@ -65,7 +59,7 @@ mod tests {
     fn test_part1() {
         assert_eq!(
             part1(fs::read_to_string("./input/1/real.txt").unwrap()),
-            "example"
+            "1036"
         );
     }
 
@@ -73,7 +67,7 @@ mod tests {
     fn test_part2() {
         assert_eq!(
             part2(fs::read_to_string("./input/1/real.txt").unwrap()),
-            "example2"
+            "6228"
         );
     }
 }
