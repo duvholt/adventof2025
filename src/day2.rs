0ded3cd252006a6d8188431c34baf0a4b25e3f3b
Christian Duvholt <christian@duvholt.net>
2025-12-02 07:04:39 +0100

    Initial day 2

---
+++ src/day2.rs
@@ -0,0 +1,82 @@
+pub fn part1(contents: String) -> String {
+    let mut eqs = 0;
+
+    for line in contents.split(',') {
+        let (first, second) = parse(line);
+
+        for i in first..=second {
+            let len = i.to_string().len();
+            let chunk =  len / 2;
+            if len != chunk * 2 {
+                continue;
+            }
+            if equal_parts(i, chunk) {
+                eqs += i;
+            }
+        }
+    }
+    eqs.to_string()
+}
+
+fn parse(line: &str) -> (i64, i64) {
+    let mut parts = line.split('-');
+
+    let first: i64 = parts.next().unwrap().trim().parse().unwrap();
+    let second: i64 = parts.next().unwrap().trim().parse().unwrap();
+    (first, second)
+}
+
+fn equal_parts(first: i64, parts: usize) -> bool {
+    if first < 10 {
+        return false
+    }
+    let first_s = first.to_string();
+    let vec: Vec<_> = first_s.chars().collect();
+    let mut chunks = vec.chunks(parts);
+    let f1 = chunks.next().unwrap();
+    chunks.all(|f2| { f2 == f1})
+}
+
+pub fn part2(contents: String) -> String {
+    let mut eqs = 0;
+
+    for line in contents.split(',') {
+        let (first, second) = parse(line);
+
+        for i in first..=second {
+            let len = i.to_string().len();
+            let chunk =  len / 2;
+            for c in 1..=chunk {
+                if equal_parts(i, c) {
+                    eqs += i;
+                    break;
+                }
+
+            }
+        }
+    }
+    eqs.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(
+            part1(fs::read_to_string("./input/2/real.txt").unwrap()),
+            "24747430309"
+        );
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            part2(fs::read_to_string("./input/2/real.txt").unwrap()),
+            "30962646823"
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 pub mod day1;
+pub mod day2;
 pub mod example;
 
 type Task = fn(String) -> String;
@@ -9,6 +10,8 @@ pub fn day_tasks() -> HashMap<&'static str, Task> {
     let mut map: HashMap<&str, Task> = HashMap::new();
     map.insert("example-1", example::part1);
     map.insert("example-2", example::part2);
+    map.insert("2-1", day2::part1);
+    map.insert("2-2", day2::part2);
     map.insert("1-1", day1::part1);
     map.insert("1-2", day1::part2);
     map
