ba66f9232da9a2bfd9759fa471694aac0e27e2b6
Christian Duvholt <christian@duvholt.net>
2025-12-04 06:36:34 +0100

    Quick refactor

--- src/day4.rs
+++ src/day4.rs
@@ -1,45 +1,17 @@
 pub fn part1(contents: String) -> String {
-    let mut map = Vec::new();
-    for row in contents.lines() {
-        let mut map_row = Vec::new();
-        for cell in row.chars() {
-            let value = match cell {
-                '@' => true,
-                '.' => false,
-                _ => panic!("Unknown"),
-            };
-            map_row.push(value);
-        }
-        map.push(map_row);
-    }
+    let map = parse(contents);
 
     let mut accessed = 0;
 
     for (y, rows) in map.iter().enumerate() {
         for (x, cell) in rows.iter().enumerate() {
+            // empty
             if !*cell {
                 continue;
             }
-            let mut boxes = 0;
-            for rel_x in -1..=1 {
-                let new_x = (x as i64) + rel_x;
-                if new_x < 0 || new_x >= rows.len() as i64 {
-                    continue;
-                }
-                let new_x = new_x as usize;
-                for rel_y in -1..=1 {
-                    let new_y = (y as i64) + rel_y;
-                    if new_y < 0 || new_y >= map.len() as i64 {
-                        continue;
-                    }
-                    let new_y = new_y as usize;
-                    let is_roll = map[new_y][new_x];
-                    if is_roll {
-                        boxes += 1;
-                    }
-                }
-            }
-            if boxes < 4 + 1 {
+
+            let rolls = adjacent_rolls(&map, y, x);
+            if rolls < 4 {
                 accessed += 1;
             }
         }
@@ -49,6 +21,36 @@ pub fn part1(contents: String) -> String {
 }
 
 pub fn part2(contents: String) -> String {
+    let mut map = parse(contents);
+
+    let mut total_accessed = 0;
+
+    loop {
+        let mut accessed = 0;
+        for y in 0..map.len() {
+            for x in 0..map[y].len() {
+                let cell = map[y][x];
+                // empty
+                if !cell {
+                    continue;
+                }
+                let rolls = adjacent_rolls(&map, y, x);
+                if rolls < 4 {
+                    accessed += 1;
+                    map[y][x] = false;
+                }
+            }
+        }
+        if accessed == 0 {
+            break;
+        }
+        total_accessed += accessed;
+    }
+
+    total_accessed.to_string()
+}
+
+fn parse(contents: String) -> Vec<Vec<bool>> {
     let mut map = Vec::new();
     for row in contents.lines() {
         let mut map_row = Vec::new();
@@ -62,48 +64,36 @@ pub fn part2(contents: String) -> String {
         }
         map.push(map_row);
     }
+    map
+}
 
-    let mut accessed = 0;
-    let mut removed = 1; // hack
-
-    while removed > 0 {
-        removed = 0;
-        for y in 0..map.len() {
-            for x in 0..map[y].len() {
-                let cell = map[y][x];
-                if !cell {
-                    continue;
-                }
-                let mut boxes = 0;
-                for rel_x in -1..=1 {
-                    let new_x = (x as i64) + rel_x;
-                    if new_x < 0 || new_x >= map[y].len() as i64 {
-                        continue;
-                    }
-                    let new_x = new_x as usize;
-                    for rel_y in -1..=1 {
-                        let new_y = (y as i64) + rel_y;
-                        if new_y < 0 || new_y >= map.len() as i64 {
-                            continue;
-                        }
-                        let new_y = new_y as usize;
-                        let is_roll = map[new_y][new_x];
-                        if is_roll {
-                            boxes += 1;
-                        }
-                    }
-                }
-                // counting current roll as well
-                if boxes < 4 + 1 {
-                    accessed += 1;
-                    removed += 1;
-                    map[y][x] = false;
-                }
+fn adjacent_rolls(map: &[Vec<bool>], y: usize, x: usize) -> i32 {
+    let mut rolls = 0;
+    for rel_x in -1..=1 {
+        let new_x = (x as i64) + rel_x;
+        // bounds
+        if new_x < 0 || new_x >= map[y].len() as i64 {
+            continue;
+        }
+        let new_x = new_x as usize;
+        for rel_y in -1..=1 {
+            // self check
+            if rel_x == 0 && rel_y == 0 {
+                continue;
+            }
+            let new_y = (y as i64) + rel_y;
+            // bounds
+            if new_y < 0 || new_y >= map.len() as i64 {
+                continue;
+            }
+            let new_y = new_y as usize;
+            let is_roll = map[new_y][new_x];
+            if is_roll {
+                rolls += 1;
             }
         }
     }
-
-    accessed.to_string()
+    rolls
 }
 
 #[cfg(test)]
