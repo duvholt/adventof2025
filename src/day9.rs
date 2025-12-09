017c9519a294d67e17414bf714edcd42db2ae9b5
Christian Duvholt <christian.duvholt@nrk.no>
2025-12-09 10:13:09 +0100

    Add corner info. Still completely broken

--- src/day9.rs
+++ src/day9.rs
@@ -1,4 +1,6 @@
-use gxhash::{HashMap, HashMapExt, HashSet, HashSetExt};
+use std::collections::HashSet;
+
+use gxhash::{HashMap, HashMapExt};
 
 pub fn part1(contents: String) -> String {
     let coordinates = parse(contents);
@@ -22,48 +24,102 @@ pub fn part1(contents: String) -> String {
 pub fn part2(contents: String) -> String {
     let red_tiles = parse(contents);
 
-    let mut green_tiles = HashSet::new();
+    let mut green_tiles: HashSet<((i64, i64), Corner)> = HashSet::new();
+    let previous_direction: Direction = {
+        let last = red_tiles[red_tiles.len() - 1];
+        let almost_last = red_tiles[red_tiles.len() - 2];
+        get_direction(almost_last, last)
+    };
     for (i1, &tile) in red_tiles.iter().enumerate() {
         let i2 = if i1 + 1 == red_tiles.len() { 0 } else { i1 + 1 };
         let other = &red_tiles[i2];
+
+        let direction = get_direction(tile, *other);
+
+        let corner = match (previous_direction, direction) {
+            (Direction::Down, Direction::Down)
+            | (Direction::Left, Direction::Left)
+            | (Direction::Right, Direction::Right)
+            | (Direction::Up, Direction::Up) => Corner::Edge,
+
+            (Direction::Down, Direction::Left) => Corner::LowerLeft,
+            (Direction::Left, Direction::Up) => Corner::LowerLeft,
+
+            (Direction::Down, Direction::Right) => Corner::LowerRight,
+            (Direction::Right, Direction::Up) => Corner::LowerRight,
+
+            (Direction::Up, Direction::Left) => Corner::UpperLeft,
+            (Direction::Left, Direction::Down) => Corner::UpperLeft,
+
+            (Direction::Up, Direction::Right) => Corner::UpperRight,
+            (Direction::Right, Direction::Down) => Corner::UpperRight,
+
+            (Direction::Up, Direction::Down) => {
+                panic!("what? up-down");
+            }
+            (Direction::Down, Direction::Up) => {
+                panic!("what? down-up");
+            }
+            (Direction::Left, Direction::Right) => {
+                panic!("what? left-right");
+            }
+            (Direction::Right, Direction::Left) => {
+                panic!("what? right-left");
+            }
+        };
+
         if tile.0 == other.0 {
             // horizontal
             let min = tile.1.min(other.1);
             let max = tile.1.max(other.1);
             for y in min..=max {
-                green_tiles.insert((tile.0, y));
+                green_tiles.insert(((tile.0, y), corner));
             }
         } else if tile.1 == other.1 {
             // vertical
             let min = tile.0.min(other.0);
             let max = tile.0.max(other.0);
             for x in min..=max {
-                green_tiles.insert((x, tile.1));
+                green_tiles.insert(((x, tile.1), corner));
             }
         } else {
             dbg!(tile, other);
         }
     }
 
-    let mut green_tiles_by_x: HashMap<i64, Vec<i64>> = HashMap::new();
-    let mut green_tiles_by_y: HashMap<i64, Vec<i64>> = HashMap::new();
+    let mut green_tiles_by_x: HashMap<i64, Vec<(i64, AxisDirection)>> = HashMap::new();
+    let mut green_tiles_by_y: HashMap<i64, Vec<(i64, AxisDirection)>> = HashMap::new();
     // dbg!(&green_tiles);
-    for &(gx, gy) in green_tiles.iter() {
+    for &((gx, gy), corner) in green_tiles.iter() {
+        let mut y_edge = match corner {
+            Corner::UpperLeft => AxisDirection::Right,
+            Corner::LowerLeft => AxisDirection::Right,
+            Corner::UpperRight => AxisDirection::Left,
+            Corner::LowerRight => AxisDirection::Left,
+            Corner::Edge => AxisDirection::Edge,
+        };
         green_tiles_by_x
             .entry(gx)
-            .and_modify(|v| v.push(gy))
-            .or_insert_with(|| vec![gy]);
+            .and_modify(|v| v.push((gy, y_edge)))
+            .or_insert_with(|| vec![(gy, y_edge)]);
 
+        let mut x_edge = match corner {
+            Corner::UpperLeft => AxisDirection::Right,
+            Corner::LowerLeft => AxisDirection::Left,
+            Corner::UpperRight => AxisDirection::Right,
+            Corner::LowerRight => AxisDirection::Left,
+            Corner::Edge => AxisDirection::Edge,
+        };
         green_tiles_by_y
             .entry(gy)
-            .and_modify(|v| v.push(gx))
-            .or_insert_with(|| vec![gx]);
+            .and_modify(|v| v.push((gx, x_edge)))
+            .or_insert_with(|| vec![(gx, x_edge)]);
     }
     for v in green_tiles_by_x.values_mut() {
-        v.sort();
+        v.sort_by_key(|(c, _)| *c);
     }
     for v in green_tiles_by_y.values_mut() {
-        v.sort();
+        v.sort_by_key(|(c, _)| *c);
     }
 
     let mut max_area = 0;
@@ -117,16 +173,59 @@ pub fn part2(contents: String) -> String {
         }
     }
 
+    let green_tiles: HashSet<(i64, i64)> = green_tiles.into_iter().map(|(c, _)| c).collect();
+
     print_map(red_tiles, green_tiles, rect);
 
     max_area.to_string()
+}
+
+fn get_direction(almost_last: (i64, i64), last: (i64, i64)) -> Direction {
+    if last.0 == almost_last.0 {
+        // horizontal
+        if last.1 > almost_last.1 {
+            Direction::Right
+        } else {
+            Direction::Left
+        }
+    } else {
+        if last.1 > almost_last.1 {
+            Direction::Up
+        } else {
+            Direction::Down
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum Corner {
+    UpperLeft,  // ┌
+    LowerLeft,  // └
+    UpperRight, // ┐
+    LowerRight, // ┘
+    Edge,       // │ or -
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum AxisDirection {
+    Left,
+    Right,
+    Edge,
 }
 
 fn check_no_gaps(
     x: i64,
     min_y: i64,
     max_y: i64,
-    green_tiles_by_x: &HashMap<i64, Vec<i64>>,
+    green_tiles_by_x: &HashMap<i64, Vec<(i64, AxisDirection)>>,
 ) -> bool {
     let mut start = true;
     let mut hole_y = true;
@@ -134,8 +233,8 @@ fn check_no_gaps(
     match green_tiles_by_x.get(&x) {
         Some(gy) => {
             for i in 0..gy.len() - 1 {
-                let gy1 = gy[i];
-                let gy2 = gy[i + 1];
+                let (gy1, _) = gy[i];
+                let (gy2, edge) = gy[i + 1];
                 let gap = gy1 + 1 != gy2;
 
                 if start {
@@ -149,7 +248,6 @@ fn check_no_gaps(
                     possible_overlap = None;
                 }
 
-
                 if !hole_y && !gap && gy1 <= min_y {
                     // println!(
                     //     "Possible start! x={}, {}-{} ({}-{})",
@@ -160,7 +258,9 @@ fn check_no_gaps(
                     }
                 }
 
-                if let Some(first_overlap) = possible_overlap && gy2 >= max_y {
+                if let Some(first_overlap) = possible_overlap
+                    && gy2 >= max_y
+                {
                     if first_overlap <= gy1 {
                         // println!(
                         //     "From possible to complete! x={}, {}-{} ({}-{})",
