dec327cad00aa452332b3512dc0db1775068b1ed
Christian Duvholt <christian@duvholt.net>
2025-12-11 07:19:28 +0100

    Implement general solution of day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 pub fn part1(contents: String) -> String {
     let mut edges = HashMap::new();
@@ -40,14 +40,8 @@ pub fn part2(contents: String) -> String {
         edges.insert(from, to);
     }
 
-    // found order by inspecting graph using graphviz :)
-    // svr -> fft -> dac -> out
-
-    let svr_fft= all_paths_from_node(&edges, "svr", "fft");
-    let fft_dac = all_paths_from_node(&edges, "fft", "dac");
-    let dac_out = all_paths_from_node(&edges, "dac", "out");
-
-    let paths = svr_fft * fft_dac * dac_out;
+    let required = vec!["dac", "fft"];
+    let paths = all_paths_from_node(&edges, "svr", "out", &required);
 
     paths.to_string()
 }
@@ -56,21 +50,27 @@ fn all_paths_from_node<'a>(
     edges: &HashMap<&'a str, Vec<&'a str>>,
     start: &'a str,
     goal: &'a str,
+    required: &Vec<&'a str>,
 ) -> usize {
-     let mut memo = HashMap::new();
-    paths_between_nodes(&mut memo, edges, start, goal)
+    let mut memo = HashMap::new();
+    paths_between_nodes(&mut memo, edges, start, goal, required)
 }
 
 fn paths_between_nodes<'a>(
-    memo: &mut HashMap<&'a str, usize>,
+    memo: &mut HashMap<(&'a str, Vec<&'a str>), usize>,
     edges: &HashMap<&'a str, Vec<&'a str>>,
     start: &'a str,
     goal: &'a str,
+    required: &Vec<&'a str>,
 ) -> usize {
-    if start == goal {
+    if start == goal && required.is_empty() {
         return 1;
     }
-    if let Some(paths) = memo.get(start) {
+
+    let mut required = required.clone();
+    required.retain(|v| *v != start);
+
+    if let Some(paths) = memo.get(&(start, required.clone())) {
         return *paths;
     }
 
@@ -78,12 +78,12 @@ fn paths_between_nodes<'a>(
 
     if let Some(node_edges) = edges.get(start) {
         for edge in node_edges {
-            let sub_paths = paths_between_nodes(memo, edges, edge, goal);
+            let sub_paths = paths_between_nodes(memo, edges, edge, goal, &required);
             paths += sub_paths;
         }
     }
 
-    memo.insert(start, paths);
+    memo.insert((start, required), paths);
     paths
 }
 
