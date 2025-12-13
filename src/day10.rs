7863ba70f3d9f5793beccc85732f40fecfd12de9
Christian Duvholt <christian@duvholt.net>
2025-12-13 16:50:03 +0100

    Work in progress system of linear equations solution

--- src/day10.rs
+++ src/day10.rs
@@ -1,6 +1,8 @@
-use rayon::prelude::*;
-use std::{collections::VecDeque, vec};
+use std::{
+    cmp::min, collections::{HashMap, VecDeque}, vec
+};
 
+use itertools::Itertools;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -75,12 +77,256 @@ pub fn part2(contents: String) -> String {
     let machines = parse(contents);
 
     let button_presses: u64 = machines
-        .par_iter()
+        .iter()
         .enumerate()
-        .map(|(i, machine)| run_machine(machine, i, machines.len()))
+        .map(|(i, machine)| gauss_machine(machine, i, machines.len()))
         .sum();
 
     button_presses.to_string()
+}
+
+fn gauss_machine(machine: &Machine, i: usize, length: usize) -> u64 {
+    println!("Initial state {:?} [{}/{}]", machine, i + 1, length);
+
+    let (mut a, n, m) = if false {
+        // let mut a = vec![
+        //     vec![2.0, 1.0, -1.0, 8.0],
+        //     vec![-3.0, -1.0, 2.0, -11.0],
+        //     vec![-2.0, 1.0, 2.0, -3.0],
+        // ];
+        let mut a = vec![
+            vec![3.0, -3.0, 3.0, 9.0],
+            vec![2.0, -1.0, 4.0, 7.0],
+            vec![3.0, -5.0, -1.0, 7.0],
+        ];
+        let n = a.len();
+        let m = a[0].len() - 1;
+        (a, n, m)
+    } else {
+        let m = machine.buttons.len();
+        let n = machine.joltage.len();
+        let mut a: Vec<Vec<f64>> = vec![vec![0.0; m + 1]; n];
+        for (m_i, button) in machine.buttons.iter().enumerate() {
+            for &n_i in button.iter() {
+                a[n_i][m_i] = 1.0;
+            }
+        }
+        for (n_i, value) in machine.joltage.iter().enumerate() {
+            a[n_i][m] = *value as f64;
+        }
+        (a, n, m)
+    };
+
+    let mut where_v: Vec<Option<usize>> = vec![None; m];
+
+    print_matrix(&a);
+
+    let mut row: usize = 0;
+    for col in 0..m {
+        // println!("iter col={col} row={row}");
+        if row >= n {
+            break;
+        }
+        let mut sel = row;
+        for i in row..n {
+            if (a[i][col].abs()) > a[sel][col].abs() {
+                sel = i;
+            }
+        }
+        // println!("Selection {}", sel);
+        // not sure about this
+        if a[sel][col].abs() < f64::EPSILON {
+            // println!("Skipping?");
+            continue;
+        }
+        for i in col..=m {
+            // swap
+            let tmp = a[sel][i];
+            a[sel][i] = a[row][i];
+            a[row][i] = tmp;
+        }
+        // println!("Swapped sel={sel} row={row}");
+        // print_matrix(&a);
+        where_v[col] = Some(row);
+
+        // not part of original
+        let c = a[row][col];
+        for j in col..=m {
+            a[row][j] /= c;
+        }
+        // println!("Reduced");
+        // print_matrix(&a);
+
+        for i in 0..n {
+            if i != row {
+                let c = a[i][col] / a[row][col];
+                // println!("c={c} i={i} row={row}");
+                for j in col..=m {
+                    a[i][j] -= a[row][j] * c
+                }
+            }
+        }
+        // println!("Subbed");
+        // print_matrix(&a);
+        row += 1;
+    }
+
+    println!("After");
+
+    print_matrix(&a);
+
+    // let mut ans: Vec<f64> = vec![0.0; m];
+
+    // for i in 0..m {
+    //     if let Some(w) = where_v[i] {
+    //         ans[i] = a[w][m] / a[w][i];
+    //     }
+    // }
+    // println!("ans={:?}", ans);
+    // for i in 0..n {
+    //     let mut sum: f64 = 0.0;
+    //     for j in 0..m {
+    //         sum += ans[j] * a[i][j];
+    //     }
+    //     if (sum - a[i][m]).abs() > 1e-9 {
+    //         println!("Infinite solutions for {}", i);
+    //         // panic!("wtf {} {}", sum, a[i][m]);
+    //     }
+    // }
+
+    println!("{:?}", where_v);
+
+    // x_1 = 2 - z_1 + z_2
+    // x_2 = 5 - z_2
+    // x_3 = 1 - z_1 + z_2
+    // x_4 = z_1
+    // x_5 = 3 - z_2
+    // x_6 = z_2
+    let mut equations: Vec<Vec<(f64, Option<usize>)>> = vec![];
+    // let x_1 = vec![(2, None), (-1, Some(0)), (1, Some(1))];
+
+    let mut free_map: HashMap<usize, usize> = HashMap::new();
+    let mut free_index = 0;
+
+    for (col, w) in where_v.iter().enumerate() {
+        if w.is_none() {
+            free_map.insert(col, free_index);
+
+            free_index += 1;
+        }
+    }
+
+    dbg!(&free_map);
+
+    for (col, w) in where_v.iter().enumerate() {
+        match w {
+            Some(row) => {
+                let mut equation: Vec<(f64, Option<usize>)> = vec![(round_float(a[*row][m]), None)];
+                let mut f = false;
+                // println!("{:?}", &a[*row]);
+                for i in 0..m {
+                    let v = a[*row][i];
+                    let fixed_v = round_float(v);
+                        
+                    
+                    if !f {
+                        if v == 1.0 {
+                            // skip until pivot
+                            f = true;
+                        }
+                        // skip pivot too
+                        continue;
+                    }
+
+                    // dbg!(i, v, fixed_v);
+
+                    if v != 0.0 {
+                        let free_index = *free_map.get(&i).unwrap();
+                        equation.push((-fixed_v, Some(free_index)));
+                    }
+                }
+                equations.push(equation);
+            }
+            None => {
+                let free_index = *free_map.get(&col).unwrap();
+                equations.push(vec![(1.0, Some(free_index))]);
+            }
+        }
+    }
+    // dbg!(&equations);
+
+
+    // find minimal solution
+
+    let mut minimum = f64::MAX;
+
+
+    let max_joltage = machine.joltage.iter().max().unwrap();
+
+    let free_combi = free_map.keys().map(|key| {
+        0..=*machine.joltage.iter().max().unwrap()
+    }).multi_cartesian_product();
+
+    'combi: for free_values in free_combi {
+        let mut button_presses = vec![];
+        for equation in equations.iter() {
+            let presses = equation.iter().map(|(value, free_var)| {
+                match free_var {
+                    Some(free_var) => value * (free_values[*free_var] as f64),
+                    None => *value,
+                }
+            }).sum::<f64>();
+            // dbg!(&presses);
+            if presses < 0.0  {
+                continue 'combi;
+            }
+            button_presses.push(presses);
+        }
+        let button_sum = button_presses.iter().sum::<f64>();
+        if button_sum < minimum {
+            println!("New best! {button_sum} {:?} {:?}", free_values, button_presses);
+            minimum = round_float(button_sum);
+            if (minimum - button_sum).abs() > 1e-9 {
+                panic!("Float sum error");
+            }
+            if button_presses.len() != machine.buttons.len() {
+                panic!("Wrong number of buttons");
+            }
+        }
+    }
+
+
+    dbg!(minimum);
+
+    if minimum == f64::MAX {
+        panic!("BROKEN!!!!");
+    }
+
+    // for i in i..m {
+    //     if where_v[i].is_none() {
+    //         panic!("wtf2");
+    //     }
+    // }
+
+    // println!("Solution found? {:?}", ans);
+
+    return minimum as u64;
+}
+
+fn round_float(v: f64) -> f64 {
+    let fixed_v = if (v - v.round()).abs() < 1e-9 {
+            v.round()
+        } else { 
+            v
+        };
+    fixed_v
+}
+
+fn print_matrix(a: &Vec<Vec<f64>>) {
+    for row in a.iter() {
+        println!("{:?}", row);
+    }
+    println!();
 }
 
 fn run_machine(machine: &Machine, i: usize, length: usize) -> u64 {
